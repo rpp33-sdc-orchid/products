f060a5163cb1fb968492b6b559e2377a83f9f5f0
yfcheng1 <b99501048@gmail.com>
2022-03-21 22:18:20 -0400

    postgres database connection and express server

---
+++ db/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { pool } = require('pg');
+
+// pools will use environment variables for connection information
+const pool = new Pool({
+  user: process.env.PGUSER || 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  database: process.env.PGDATABASE || 'postgres',
+  password: process.env.PGPASSWORD || '',
+  port: process.env.PGPORT || 5432
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
