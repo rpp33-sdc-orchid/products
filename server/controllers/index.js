f362919d4da71a75cd1d39e5d43f4ca5571b9664
yfcheng1 <b99501048@gmail.com>
2022-03-27 00:33:37 -0400

    work on product list route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,17 @@
 const models = require('../models');
 
 module.exports = {
-  getProductList: () => {},
+  getProductList: (req, res) => {
+    // TODO: get page and count from req.param?
+    models.getProductList(page, count)
+      .then((res) => {
+        // TODO: send result to client
+      })
+      .catch((err) => {
+        console.log('error getting product list', err);
+        res.sendStatus(500);
+      })
+  },
   getProduct: () => {},
   getStyles: () => {},
   getRelated: (req, res) => {
@@ -11,7 +21,7 @@ module.exports = {
         // TODO: send result to client
       })
       .catch((err) => {
-        console.log('error getting related product in server', err);
+        console.log('error getting related product', err);
         res.sendStatus(500);
       })
   }
--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,38 @@
 const pool = require('../../db/index.js');
 
 module.exports = {
-  getProductList: () => {},
+  getProductList: (page = 1, count = 5) => {
+    console.log('product page and count?', page, count);
+    const query = `SELECT * FROM products LIMIT $2 OFFSET $1`;
+
+    return pool.connect().then((client) => {
+      return client.query(query, [page * count - count, count])
+        .then((res) => {
+          client.release();
+          console.log('results?', res.rows);
+          // TODO: return results?
+        })
+        .catch((err) => {
+          console.log('error executing query', err);
+        })
+    });
+  },
   getProduct: () => {},
   getStyles: () => {},
   getRelated: (productId) => {
     console.log('product id in related?', productId);
     const query = `SELECT json_agg(related.related_product_id) FROM related WHERE related.product_id = $1`
 
-    pool.connect().then((client) => {
-      client.query(query, [productId])
+    return pool.connect().then((client) => {
+      return client.query(query, [productId])
         .then((res) => {
           client.release();
           console.log('results?', res.rows);
+          // TODO: return results?
         })
         .catch((err) => {
           console.log('error executing query', err);
         })
-      });
-    };
+    });
+  }
 };
