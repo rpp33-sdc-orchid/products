1e47bb8803d8cbb0b491e19513c69255876543f4
yfcheng1 <b99501048@gmail.com>
2022-03-26 22:21:46 -0400

    modify default database

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const { pool } = require('pg');
 const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   host: process.env.PGHOST || 'localhost',
-  database: process.env.PGDATABASE || 'postgres',
+  database: process.env.PGDATABASE || 'sdc_products',
   password: process.env.PGPASSWORD || '',
   port: process.env.PGPORT || 5432
 });
