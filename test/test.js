679ce4ad3d3d74093d18501ecb5550e4a57964d1
yfcheng1 <b99501048@gmail.com>
2022-04-05 22:42:38 -0400

    refactor test suite and add more test

--- test/test.js
+++ test/test.js
@@ -23,53 +23,91 @@ describe('Test on test suite setup', () => {
 });
 
 
-describe('Test on API end points', () => {
+describe('Test on GET/products API end point', () => {
 
-  const testProductId = 1;
-
-  test('should get product lists from API', async () => {
+  test('should get product lists from API without query', async () => {
     const result = await request(app)
       .get('/products')
       // .set('Authorization', API_KEY)
-      .query({page: 1, count: 5})
-      .catch((err) => console.log('err on GET/products', err));
+      .catch((err) => console.log('err on TEST GET/products', err));
 
     expect(result.status).toBe(200);
     expect(Array.isArray(result._body)).toBe(true);
-    expect(result._body.length).toBeGreaterThan(0);
+    expect(result._body.length).toBe(5);
   });
+
+  test('should get product lists from API with specific query page and count', async () => {
+    const result = await request(app)
+      .get('/products')
+      // .set('Authorization', API_KEY)
+      .query({page: 1, count: 10})
+      .catch((err) => console.log('err on TEST GET/products', err));
+
+    expect(result.status).toBe(200);
+    expect(Array.isArray(result._body)).toBe(true);
+    expect(result._body.length).toBe(10);
+  });
+
+});
+
+describe('Test on GET/products/:product_id API end point', () => {
+
+  const testProductId = 1;
+  const errorProductId = 0;
 
   test('should get product information from API', async () => {
     const result = await request(app)
       .get(`/products/${testProductId}`)
       // .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET/products/:product_id', err));
+      .catch((err) => console.log('err on TEST GET/products/:product_id', err));
 
     expect(result.status).toBe(200);
     expect(result._body.id).toBe(testProductId);
     expect(Array.isArray(result._body.features)).toBe(true);
   });
 
+});
+
+describe('Test on GET/products/:product_id/styles API end point', () => {
+
+  const testProductId = 1;
+
   test('should get styles from API', async () => {
     const result = await request(app)
       .get(`/products/${testProductId}/styles`)
       // .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET/products/:product_id/styles', err));
+      .catch((err) => console.log('err on TEST GET/products/:product_id/styles', err));
 
     expect(result.status).toBe(200);
     expect(result._body.product_id).toBe(testProductId);
     expect(Array.isArray(result._body.results)).toBe(true);
   });
 
+});
+
+describe('Test on GET/products/:product_id/related API end point', () => {
+
+  const testProductId = 1;
+  const errorProductId = 0;
+
   test('should get related products from API', async () => {
     const result = await request(app)
       .get(`/products/${testProductId}/related`)
       // .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET/products/:product_id/related', err));
+      .catch((err) => console.log('err on TEST GET/products/:product_id/related', err));
 
     expect(result.status).toBe(200);
     expect(Array.isArray(result._body)).toBe(true);
   });
 
+  test('should send empty array to the server when id is invalid', async () => {
+    const result = await request(app)
+      .get(`/products/${errorProductId}/related`)
+      // .set('Authorization', API_KEY)
+      .catch((err) => console.log('err on TEST GET/products/:product_id/related', err));
 
+    expect(result.status).toBe(200);
+    expect(Array.isArray(result._body)).toBe(true);
+    expect(result._body.id).toBeUndefined();
+  });
 });
