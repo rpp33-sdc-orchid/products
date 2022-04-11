2b86e251514a5ec18b230a106d66de5c42cba091
yfcheng1 <b99501048@gmail.com>
2022-04-10 23:14:06 -0400

    progress save

--- db/ETL.js
+++ db/ETL.js
@@ -2,6 +2,7 @@ const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const createCsvStringifier = require("csv-writer").createObjectCsvStringifier;
 const CombinedStream = require('combined-stream');
+const Transform = require("stream").Transform;
 const fs = require('fs');
 const results = [];
 
@@ -14,20 +15,24 @@ const csvStringifier = createCsvStringifier({
   ]
 });
 
-// combine stream
-const combinedStream = CombinedStream.create();
-for (var i = 1; i <= 114; i++) {
-  combinedStream.append(
-    fs.createReadStream(`clean_data/splitcsv/photos-${i}.csv`)
-  );
-}
+// const csvWriter = createCsvWriter({
+//   path: 'clean_data/photos_test.csv',
+//   header: [
+//     {id: 'id', title: 'id'},
+//     {id: 'styleId', title: 'styleId'},
+//     {id: 'url', title: 'url'},
+//     {id: 'thumbnail_url', title: 'thumbnail_url'}
+//   ]
+// });
 
-// define writer
-const writeStream = fs.createWriteStream(
-  'clean_data/photos_test.csv'
-);
-// write header
-writeStream.write(csvStringifier.getHeaderString());
+// combine stream
+// const combinedStream = CombinedStream.create();
+// for (var i = 1; i < 10; i++) {
+//   combinedStream.append(
+//     fs.createReadStream(`clean_data/splitcsv/photos-${i}.csv`)
+//   );
+// }
+// combinedStream.append(fs.createReadStream('clean_data/splitcsv/photos-1.csv'));
 
 
 const transformer = (row) => {
@@ -49,15 +54,17 @@ const transformer = (row) => {
 };
 
 
-combinedStream
-.pipe(csv())
-.on('data', (row) => {
-  transformer(row);
-})
-.on('end', () => {
-  writeStream.write(csvStringifier.stringifyRecords(results));
-  console.log('done');
-})
+// combinedStream
+// .pipe(csv())
+// // .pipe(transformer)
+// // .pipe(writeStream)
+// .on('data', (row) => {
+//   transformer(row);
+// })
+// .on('end', () => {
+//   writeStream.write(csvStringifier.stringifyRecords(results));
+//   console.log('done');
+// })
 
 
 
@@ -67,25 +74,41 @@ combinedStream
 //     console.log('header?', headers);
 // })
 // .on('data', (row) => {
-//   // let id = parseInt(row.id);
-//   if (row.thumbnail_url.split('\n').length > 1) {
-//     results.push({
-//       id: row.id,
-//       styleId: row.styleId,
-//       url: row.url,
-//       thumbnail_url: row.thumbnail_url.split('\n').slice(0, 1)[0]
-//     });
-//   } else {
-//     results.push({
-//       id: row.id,
-//       styleId: row.styleId,
-//       url: row.url,
-//       thumbnail_url: row.thumbnail_url
-//     });
-//   }
+//   transformer(row);
 // })
 // .on('end', () => {
 //   // console.log('result?', results);
-//   writeStream.write(csvStringifier.stringifyRecords(results));
+//   csvWriter.writeRecords(results);
 //   console.log('done');
 // });
+
+
+// define writer
+const writeStream = fs.createWriteStream(
+  'clean_data/photos_test.csv', { autoClose: false }
+);
+// write header
+writeStream.write(csvStringifier.getHeaderString());
+
+// combine stream
+const combinedStream_1 = CombinedStream.create();
+for (var i = 1; i < 20; i++) {
+  combinedStream_1.append(
+    fs.createReadStream(`clean_data/splitcsv/photos-${i}.csv`)
+  );
+}
+
+combinedStream_1
+.pipe(csv())
+.on('headers', (headers) => {
+    console.log('header?', headers);
+})
+.on('data', (row) => {
+  transformer(row);
+})
+.on('end', () => {
+  // console.log('result?', results);
+  // csvWriter.writeRecords(results);
+  writeStream.write(csvStringifier.stringifyRecords(results));
+  console.log('done');
+});
