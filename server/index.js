b307f2efada7beaf9c159ee0d70b483a3de24268
yfcheng1 <b99501048@gmail.com>
2022-04-24 10:21:34 -0400

    add loaderio

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const router = require('./router.js');
+const path = require('path');
 require('dotenv').config();
+
+app.use(express.static(path.resolve(__dirname, '../loader')));
 
 app.use('/products', router);
 
